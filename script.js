6e8622b0c8464be53ec6c7e91565e42aec2fa151
Mohammed <mhammed.avez@gmail.com>
2021-08-23 21:19:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+
+const triggers = document.querySelectorAll('li')
+const nav = document.querySelector('.top')
+const background = document.querySelector('.dropdownBackground')
+
+
+const allDropdown = document.querySelectorAll('.dropdown');
+
+
+
+function handleEnter(){
+    
+
+        this.classList.add('trigger-enter')
+        background.classList.add('open')
+        setTimeout( () => {
+            this.classList.add('trigger-enter-active')
+            
+        } , 150)
+
+
+        const dropdown = this.querySelector('.dropdown');
+
+        const dropdownCoords = dropdown.getBoundingClientRect()
+        const navCoords = nav.getBoundingClientRect()
+
+        const coords = {
+
+            height : dropdownCoords.height,
+            width : dropdownCoords.width,
+
+            top : dropdownCoords.top - navCoords.top,
+            left : dropdownCoords.left - navCoords.left,
+            right: dropdownCoords.right,
+            bottom: dropdownCoords.bottom
+        }
+
+
+        background.style.setProperty('width' , coords.width + 'px')
+        background.style.setProperty('height' , coords.height + 'px')
+        background.style.setProperty('transform', `translate(${coords.left}px ,${coords.top}px)`)
+
+
+
+        // background.style.setProperty('top' , coords.top + 'px')
+        // background.style.setProperty('left' , coords.left + 'px')
+
+
+        // dropdown.style.display = 'block'
+        
+ }
+    
+    
+function handleLeave(){
+    this.classList.remove('trigger-enter', 'trigger-enter-active')
+    background.classList.remove('open')
+
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave))
+
